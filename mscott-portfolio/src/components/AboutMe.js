350f83cfc2f2ac1bad91354d6c5d6ecac85495ef
Michelle Scott <scottmm374@gmail.com>
2020-01-10 16:59:16 -0500

    chart added

--- mscott-portfolio/src/components/AboutMe.js
+++ mscott-portfolio/src/components/AboutMe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import styled from "styled-components";
+import ChartImg from "../assets/images/Chart.png";
 
 const ColorDiv = styled.div`
   // background: #407c7f;
@@ -20,11 +21,8 @@ const NewH1 = styled.h1`
   margin-left: 40%;
 `;
 
-const NewH4 = styled.h4`
-  font-size: 2rem;
-  text-align: center;
-  margin-bottom: 5%;
-  //   margin-left: 40%;
+const NewImg = styled.p`
+  width: 80%;
 `;
 
 const AboutMe = () => {
@@ -34,22 +32,9 @@ const AboutMe = () => {
         <NewH1>About Me</NewH1>
       </Row>
       <Row>
-        <Col xl="6" sm="6">
-          <NewH4>Creative</NewH4>
+        <Col>
           <p>
-            Officia sunt est anim laboris nostrud cupidatat ad amet nisi
-            laborum. Velit irure dolor elit qui adipisicing ipsum laborum id.
-            Quis magna non aliquip cupidatat officia quis labore laboris enim
-            dolor laboris.
-          </p>
-        </Col>
-        <Col xl="6" sm="6">
-          <NewH4>Logical</NewH4>
-          <p>
-            Officia sunt est anim laboris nostrud cupidatat ad amet nisi
-            laborum. Velit irure dolor elit qui adipisicing ipsum laborum id.
-            Quis magna non aliquip cupidatat officia quis labore laboris enim
-            dolor laboris.
+            <img src={ChartImg} alt="chart" />
           </p>
         </Col>
       </Row>
